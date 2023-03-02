54989b086f395e2d70fe92ea39b9ce21bde24273
zdyje <108900963+zedyje82@users.noreply.github.com>
2023-03-02 09:22:12 -0300

    Update autostart.sh

--- qtile/autostart.sh
+++ qtile/autostart.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
 
-nvidia-settings -l &
-udiskie -A -t &
-/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
+function run {
+  if ! pgrep $1 ;
+  then
+    $@&
+  fi
+}
+
+run nvidia-settings -l &
+run udiskie -A -t &
+run /usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
