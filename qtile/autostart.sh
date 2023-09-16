840f40ca544037915acfabb762ae7b8e1a7417fa
zdyje <108900963+zedyje82@users.noreply.github.com>
2023-09-15 22:59:11 -0300

    Update autostart.sh

--- qtile/autostart.sh
+++ qtile/autostart.sh
@@ -9,6 +9,5 @@ function run {
 
 run xrandr --output DP-0 --mode 1920x1080 --rate 75 &
 run nvidia-settings -l &
-run udiskie -A -t &
 run dunst &
 run /usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
